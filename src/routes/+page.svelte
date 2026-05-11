9773a0c67eaafc793230d195577d652c5a18a685
Nicholas McDonald <c00308788@setu.ie>
2026-05-11 20:08:42 +0100

    Small change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -95,7 +95,7 @@
     <section class="about-teaser">
         <h2>Want to know more about me?</h2>
         <p>
-            Visit my 'About Me' page below to find out anything you wish to know.
+            Visit my 'About Me' page below to find more about who I am.
         </p>
         <a href="{resolve('/about')}" class="learn-more">This Way ></a>
     </section>
