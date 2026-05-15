9296c5c4c758abb99cdc10a6ab4762b5955b79d4
Nicholas McDonald <c00308788@setu.ie>
2026-05-15 16:28:05 +0100

    Cut down catalogue to fix synergy issues

--- src/routes/catalogue/[title]/+page.server.js
+++ src/routes/catalogue/[title]/+page.server.js
@@ -2,19 +2,8 @@ export async function entries() {
 	return [
 		{ title: "Withered Foxy Zine" },
 		{ title: "Metal Sonic Painting" },
-		{ title: "Sketch" },
-		{ title: "Sketch" },
-		{ title: "Sketch" },
-		{ title: "Weekly Reflection" },
-		{ title: "Obstacle Reframing" },
-		{ title: "One-Meal Fasting Practice" },
-		{ title: "Power Posture Drills" },
-		{ title: "Visualisation Ritual" },
-		{ title: "Cold Exposure Start" },
-		{ title: "The 5-Minute Mind Sweep" },
-		{ title: "Honor Code Exercise" },
-		{ title: "Gratitude Bow Practice" },
-		{ title: "Tea Ceremony Lite" }
+		{ title: "Into the Pit" },
+		{ title: "Outside Your Window" },
 		// Add all valid catalogue IDs here
 	];
 }
